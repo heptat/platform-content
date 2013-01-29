0839e485c5ce0216d93c6be24aa2ae8c64cf204f
heptat <heptat@gmail.com>
2013-01-29 17:10:32 +0800

    add collections

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ require 'rack-flash'
 require 'digest/sha1'
 require 'sinatra/reloader' if :development?
 
-# require_relative 'lib/helpers'
-# Dir[File.dirname(__FILE__) + '/models/*'].each {|file| require_relative file }
+require_relative 'lib/helpers'
+Dir[File.dirname(__FILE__) + '/models/*'].each {|file| require_relative file }
 
 Mongoid.load!("config/mongoid.yml")
 
@@ -26,7 +26,7 @@ configure :production do
 end
 set :mongo_logfile, File.join("log", "mongo-driver-#{settings.environment}.log")
 
-use Rack::Session::Cookie, :secret => 'thisisasecret'
+use Rack::Session::Cookie, :secret => 'thisisadifferentsecret'
 use Rack::Flash
 
 enable :sessions
@@ -43,5 +43,29 @@ end
 
 get '/' do
   erb :index
+end
+
+get '/collections/:uid' do
+  if session[:uid].nil?
+    token_value = request.cookies["token"]
+    if token_value.nil?
+      # TODO this needs to know where to redirect to...if that hasn't been
+      # supplied in a callback then it should be a 404
+      redirect 'http://app.platform.local'
+    end
+    @token = verify_token(token_value)
+    if @token[:value] == false
+      # TODO this needs to know where to redirect to...if that hasn't been
+      # supplied in a callback then it should be a 404
+      redirect 'http://app.platform.local'
+    end
+    # token is genuine so set session:
+    session[:uid] = @token["uid"]
+  end
+  @current_user = User.where(:uid => session[:uid]).first
+  # TODO this needs to ask the app what collections this user is allowed to
+  # access
+  @collection = Collection.where(:uid => params[:uid]).first
+  erb :'collections/index'
 end
 
